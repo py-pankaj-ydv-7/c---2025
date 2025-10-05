6380cae225ac75206376fc34da35d65b8ef9a68c
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-10-06 01:03:11 +0530

    c++ functions

---
+++ functions/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int checkTemperature(int tempreture){
+        return tempreture;   
+    }
+int main (){
+    cout<<"Hello functons !";
+   
+    int temp= checkTemperature(49);
+    cout<<"tempereature is :"<<temp;
+
+    return 0;
+}
