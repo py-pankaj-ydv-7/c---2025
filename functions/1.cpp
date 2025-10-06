efc0790ff45caaffa5d7d0806a90080a8e265452
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-10-06 08:50:46 +0530

    functon intro basic stuf, like declaration , calling, defination

--- functions/1.cpp
+++ functions/1.cpp
@@ -3,11 +3,35 @@ using namespace std;
  int checkTemperature(int tempreture){
         return tempreture;   
     }
+
+    //declaration
+void serveChai(int cups);
+void makeChai(){
+    cout<< "Boiling Water, adding tea leaves, strainning...";
+}
+
+void serveChai(string teaType ="  Masala Tea " ){
+    cout<<"Serving"<<teaType<<endl;
+
+}
+
 int main (){
     cout<<"Hello functons !";
    
-    int temp= checkTemperature(49);
-    cout<<"tempereature is :"<<temp;
+    int temp = checkTemperature(49);
+    cout<<endl;
+    // cout << "tempereature is :" << temp << endl;
+    // makeChai(); // calling a function
+    cout<<endl;
+    // serveChai(8);
+    // serveChai(" Lemon Tea");
+    serveChai();
+
 
     return 0;
+}
+
+//defination of function
+void serveChai (int cups){
+    cout<<" Serving "<< cups <<" of chai ";
 }
