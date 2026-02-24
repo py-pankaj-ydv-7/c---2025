76d6b10910b4ea67f2cf7c1e6d040a05858d6426
Pankaj Yadav <pankajyadav851685@gmail.com>
2026-02-24 16:14:27 +0530

    update revise basic c++

---
+++ 28 patterns of dsa/phase 2/1pattern.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+// #include <bits/c++.h>
+using namespace std;
+int main(){
+    cout<<"Hello Pattern";
+}
---
+++ c++ one shot/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Pankaj Yadav"<<endl;
+
+    int a, b, c;
+    short sa = 9;
+    // vartable declaration and initialization rule.
+    // int _py = 45;
+    // cout<<_py<<endl;
+    // cout<<sa;
+    // cout<<endl;
+
+    // camelCase Notation 
+
+    // int marksInMaths = 83;
+    // cout<<"The marks of the student in maths is "<<marksInMaths<<endl;
+    // cout<<endl;
+
+    //DataType :-
+    // int , long, long long 
+    // float , double , long double
+    // char
+
+    float const score = 45.32; // agar me const float kr dunga to ye value ko change nahi krne dega . error dega
+    double score2 = 45.326;
+    long double score3 = 4646.46664666;
+    // score = 6666.76;
+
+
+    cout<<"The score is "<<score<<endl;
+    cout<<"The score2 is "<<score2<<endl;
+    cout<<"The score3 is "<<score3<<endl;
+
+
+    return 0;
+}
